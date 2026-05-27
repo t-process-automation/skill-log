331675c79901e67c5c5f3366a1ce39354e1eeb60
Takemoto Tomohito <tomohito.takemoto@stream.co.jp>
2026-05-28 01:37:19 +0900

    Remove temporary admin creation

--- build.sh
+++ build.sh
@@ -2,16 +2,3 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-
-python manage.py shell << EOF
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser(
-        'admin',
-        'admin@example.com',
-        'Admin12345'
-    )
-EOF
