8aac719bcddf751798a6206f28a697934157fc59
Takemoto Tomohito <tomohito.takemoto@stream.co.jp>
2026-05-28 01:25:42 +0900

    Add render superuser

--- build.sh
+++ build.sh
@@ -2,3 +2,16 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin',
+        'admin@example.com',
+        'YourPassword123'
+    )
+EOF
