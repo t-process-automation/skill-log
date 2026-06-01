5735dc721f4d75d7266ec6c427d4deaa3cfb9571
Takemoto Tomohito <tomohito.takemoto@stream.co.jp>
2026-06-01 11:26:43 +0900

    Add admin user

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
 
-python manage.py collectstatic --noinput
-python manage.py migrate
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        'admin',
+        'admin@example.com',
+        'Admin12345'
+    )
+EOF
