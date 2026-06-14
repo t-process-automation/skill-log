e26f19c305de55a53a23447813ecdfc03237e6e0
Takemoto Tomohito <tomohito.takemoto@stream.co.jp>
2026-06-14 15:38:51 +0900

    Add postgres support

--- posts/forms.py
+++ posts/forms.py
@@ -12,6 +12,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body']
+        labels = {'body': ''}
+        widgets = {
+            'body': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Write a comment...'
+            })
+        }
 
 
 class ProfileForm(forms.ModelForm):
